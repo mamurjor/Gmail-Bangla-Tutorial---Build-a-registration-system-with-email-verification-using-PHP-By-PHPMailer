c00132455a5ca6cfe6a25073c8918f9aab998599
hadijaman <jaman@gmail.com>
2023-06-01 01:11:24 +0600

    test

--- sendEmail.php
+++ sendEmail.php
@@ -342,10 +342,7 @@ use PHPMailer\PHPMailer\Exception;
             <div class="footer">
               <table border="0" cellpadding="0" cellspacing="0">
                 <tr>
-                  <td class="content-block">
-                    <span class="apple-link">Coloured.com.ng | Feminism | Culture | Law | Feminists Rising</span>
-                    <br> Don't like these emails? <a href="#">Unsubscribe</a>.
-                  </td>
+               
                 </tr>
                 <tr>
                   <td class="content-block powered-by">
